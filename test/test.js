36f69eb2e1313712e77b661273f4304cfbf2cb25
anatolsommer <anatol@anatol.at>
2016-02-06 23:56:00 +0100

    first implementation

--- test/test.js
+++ test/test.js
@@ -1,9 +1,52 @@
-var assert=require('assert'), gelth=require('..');
+var assert=require('assert'), fs=require('fs'), Gelth=require('..');
 
 describe('Gelth', function() {
+  var file;
 
-  it('should', function() {
-    gelth();
+  this.timeout(2000);
+  this._slow=1000;
+
+  before(function(done) {
+    var i, text='';
+    for (i=0; i<100; i++) {
+      text+='Line '+i+'\n';
+    }
+    fs.writeFile(__dirname+'/test.txt', text, done);
+  });
+
+  it('should read 3 lines', function(done) {
+    var lines='', cnt=0;
+    file=new Gelth(__dirname+'/test.txt', {lines:3});
+    file.on('data', function(line) {
+      lines+=line+'|';
+      if (++cnt>=3) {
+        assert.equal(lines, 'Line 97|Line 98|Line 99|');
+        done();
+      }
+    });
+  });
+
+  it('should read 3 lines and follow', function(done) {
+    var lines='', cnt=0;
+    file=new Gelth(__dirname+'/test.txt', {lines:3, follow:true});
+    setTimeout(function() {
+      fs.appendFile(__dirname+'/test.txt', 'Newline\n');
+    }, 800);
+    file.on('data', function(line) {
+      lines+=line+'|';
+      if (++cnt>=4) {
+        assert.equal(lines, 'Line 97|Line 98|Line 99|Newline|');
+        done();
+      }
+    });
+  });
+
+  it('should emit cut event', function(done) {
+    fs.unlink(__dirname+'/test.txt');
+    file.on('cut', function() {
+      file.end();
+      done();
+    });
   });
 
 });
