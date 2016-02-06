57c8c44a3035108aac66db8b79203b9f370b2d86
anatolsommer <anatol@anatol.at>
2016-02-07 00:38:20 +0100

    code improvements

--- test/test.js
+++ test/test.js
@@ -3,19 +3,21 @@ var assert=require('assert'), fs=require('fs'), Gelth=require('..');
 describe('Gelth', function() {
   var file;
 
-  this.timeout(2000);
-  this._slow=1000;
+  this.timeout(1000);
+  this._slow=500;
 
   before(function(done) {
-    var i, text='';
+    fs.unlink(__dirname+'/test.txt', function() {
+      done();
+    });
+  });
+
+  it('should read and emit 3 lines', function(done) {
+    var text='', lines='', cnt=0, i;
     for (i=0; i<100; i++) {
       text+='Line '+i+'\n';
     }
-    fs.writeFile(__dirname+'/test.txt', text, done);
-  });
-
-  it('should read 3 lines', function(done) {
-    var lines='', cnt=0;
+    fs.writeFileSync(__dirname+'/test.txt', text);
     file=new Gelth(__dirname+'/test.txt', {lines:3});
     file.on('data', function(line) {
       lines+=line+'|';
@@ -26,16 +28,16 @@ describe('Gelth', function() {
     });
   });
 
-  it('should read 3 lines and follow', function(done) {
+  it('should read 2 lines and follow', function(done) {
     var lines='', cnt=0;
-    file=new Gelth(__dirname+'/test.txt', {lines:3, follow:true});
+    file=new Gelth(__dirname+'/test.txt', {lines:2, follow:true});
     setTimeout(function() {
-      fs.appendFile(__dirname+'/test.txt', 'Newline\n');
-    }, 800);
+      fs.appendFile(__dirname+'/test.txt', 'Newline');
+    }, 100);
     file.on('data', function(line) {
       lines+=line+'|';
-      if (++cnt>=4) {
-        assert.equal(lines, 'Line 97|Line 98|Line 99|Newline|');
+      if (++cnt>=3) {
+        assert.equal(lines, 'Line 98|Line 99|Newline|');
         done();
       }
     });
@@ -47,6 +49,26 @@ describe('Gelth', function() {
       file.end();
       done();
     });
+  });
+
+  it('should emit error from stream', function(done) {
+    file.on('error', function() {
+      done();
+    });
+    file._stream.emit('error');
+  });
+
+  it('should emit error if file does not exist', function(done) {
+    file=new Gelth(__dirname+'/test.txt');
+    file.on('error', function(err) {
+      file.end();
+      done();
+    });
+  });
+
+  it('should use default buffer size', function() {
+    file=new Gelth(__filename, {lines:0});
+    assert.equal(file.opts.buffer, 32768);
   });
 
 });
